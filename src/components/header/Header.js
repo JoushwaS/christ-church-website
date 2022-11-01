17568d83ec3c835011240943ab6f77a3b21fc883
JoushwaS <joushwashahzad1@gmail.com>
2022-11-01 21:33:53 +0500

    pushing for deploy

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -100,6 +100,9 @@ import Button from "react-bootstrap/Button";
 // }
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const handleNavCollapse = () => {
+    setOpen(!open);
+  };
 
   const headerRoutes = [
     {
@@ -225,15 +228,19 @@ const Header = () => {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+          // data-bs-toggle="collapse"
+          // data-bs-target="#navbarSupportedContent"
+          // aria-controls="navbarSupportedContent"
+          // aria-expanded="false"
+          // aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className={`${open ? "collapse" : ""} navbar-collapse`}
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav me-auto d-flex  ">
             {headerRoutes.map((item) => {
               return (
